e6780e21d92556def6ef0ed426a3f981c09dc0d3
Airish Reyes <reyes.air30@gmail.com>
2023-05-03 16:20:10 +0100

    changing drawing

--- java/src/oopBaddies/Airish.java
+++ java/src/oopBaddies/Airish.java
@@ -20,33 +20,10 @@ public class Airish extends Visual {
     int j=1;
     int n=300;
     int a=1;
-    float z = 0;
-
-
-    public void mount(int a, int b, int c, float d)
-    {
-            //bird left side of sky
-            airish.stroke(0);
-            airish.line(200,80,160,100);       
-            airish.line(160,80,160,100);
-
-            //bird upper right in sky
-            airish.translate(450,-7);
-            airish.line(200,80,160,100);
-            airish.line(160,80,160,100);
-
-            //mountains
-            airish.triangle(45,400,500,400,250,50);
-            airish.triangle(100,400,500,400,400,20);
-            
-    }
-
-
-    
-
 
     void render()
     {
+
         //calulating average
         float avg = 0;
         for (int i = 0; i < ab.size(); i++)
@@ -56,141 +33,162 @@ public class Airish extends Visual {
         avg = avg / ab.size();
         float smoothedavg = 0;
         smoothedavg = lerp(smoothedavg, avg, 0.1f);
-    
-        airish.colorMode(RGB);
 
+        //make colours to music
         float c = map(avg, -1, 1, 0, 255);
 
-
-
-         //mountains
-         mount(45,400,500,400);
-        //mountains
-        mount(45,400,500,400);
-        //mount(-100,400,500,400 );
-
-
-
-
-        //bird left side of sky
-        airish.line(200,80,160,100);
-        airish.line(160,80,160,100);
-        
-        //sun in sky
-        airish.pushMatrix();
-        
-        airish.translate(40,80);
-        airish.fill(245, 187, 87);
-        airish.stroke(245, 187, 87);
-
-
-  
-        airish.translate(40,80);
-        airish.fill(245, 187, 87);
-        airish.stroke(245, 187, 87); 
-        airish.translate(500,40);
-        airish.rotate(radians(frameCount / 2));
-        airish.ellipse(0, 0, 60, 60);
-        airish.line(0, -60, 0, -40);
-        airish.line(0, 40, 0, 60);
-        airish.line(-45, -45, -30, -30);
-        airish.line(45, -45, 30, -30);
-        airish.line(-60, 0, -40, 0);
-        airish.line(40, 0, 60, 0);
-        airish.line(-45, 45, -30, 30);
-        airish.line(45, 45, 30, 30);
-        airish.popMatrix();
-        airish.noFill();
-        
-        //bird upper right in sky
-        airish.translate(450,-7);
-        airish.line(200,80,160,100);
-        airish.line(160,80,160,100);
-
-
-
-
-
-
-
-
-
-
-
-         //ferriswheel
-        // ferriswheel(0,0,200,200);
-       //  mount(-100,400,500,400 );
-
-        //borderline colour of mountains
-        airish.stroke(211);
-        //inside colour of mountains
-        airish.fill(255);
-
-        airish.pushMatrix();
-
-        //airi
-        //main wheel of ferriswheel
-        airish.ellipse(0, -40, 50, 50);
-
-        //frame
-        airish.triangle(0,0,75,130,-75,130);
-
-        airish.popMatrix();
-
-        //Spokes in wheel
-        for(int i = 0; i < 18; i++)
+        airish.background(0);//black background
+        airish.noStroke();
+        //fill the middle circles in white
+        airish.fill(255,255,255);
+        //if a is 1 meaning this will only appear if user presses key that allows to display this drwaing
+        if(a==1)
         {
-            airish.rotateZ(PI/9);
-            airish.line(0,0,0,100);
-        }
+            //we put the centre circle at the start as the petals of flower will overlap it
+            //draws the centre white circle of flower
+            airish.fill(255,255,255);
+            airish.ellipse(250,250,100,100);
 
-        //carriages from ferris wheel
-        for(int j = 0; j<6; j++)
-        {
-            airish.pushMatrix();
-            airish.rotate(PI/3);
-            airish.translate(0,100,0);
-            airish.rotate(-z);
             
-            airish.rotateZ(radians(-60*j));
-            airish.rotateZ(radians(-60));
-            airish.fill(10,150,255);
-            airish.triangle(0,0,5,10,-5,10);
-            airish.fill(255,150,10);
+            airish.fill(253, 47, 96);
+            airish.ellipse(175,175,50,50);
+            airish.triangle(175,200,250,250,200,175);
 
-            airish.popMatrix();
-            airish.rotate(z);
-            airish.translate(0, 900, 0);
+            airish.ellipse(250,150,50,50);
+            airish.triangle(255,150,250,250,275,150);
 
-           // airish.popMatrix();
-        }
+            airish.ellipse(325,175,50,50);
+            airish.triangle(300,175,250,250,325,200);
 
-        z = z-(PI/700);
-        println("X: " + mouseX + " Y: " + mouseY);
+            airish.ellipse(150,250,50,50);
+            airish.triangle(150,255,250,250,150,275);
+
+            airish.ellipse (350,250,50,50);
+            airish.triangle(350,225,250,250,350,275);
+
+            airish.ellipse(175,325,50,50);
+            airish.triangle(175,300,250,250,200,325);
+
+            airish.ellipse(250,350,50,50);
+            airish.triangle(225,350,250,250,275,350);
+
+            airish.ellipse(325,325,50,50);
+            airish.triangle(300,325,250,250,325,300);
+
+        }//end if
+
+        if(a>=3)
+        {
+            for (int i= -250;i<1400; i+=250)
+            {
+                for (j=-250;j<700;j+=250)
+                {
+                    airish.fill(255);
+                    airish.ellipse(250+i,250+j,100,100); 
+                    airish.fill (c, 100 ,100);
+                    airish.ellipse(175+i,175+j,50,50);
+                    airish.triangle(175+i,200+j,250+i,250+j,200+i,175+j);
+                    airish.ellipse(250+i,150+j,50,50);
+                    airish.triangle(225+i,150+j,250+i,250+j,275+i,150+j);
+
+                    airish.ellipse(325+i,175+j,50,50);
+                    airish.triangle(300+i,175+j,250+i,250+j,325+i,200+j);
+
+                    airish.ellipse(150+i,250+j,50,50);
+                    airish.triangle(150+i,225+j,250+i,250+j,150+i,275+j);
+
+                    airish.ellipse(350+i,250+j,50,50);
+                    airish.triangle(350+i,225+j,250+i,250+j,350+i,275+j);
+
+                    airish.ellipse(175+i,325+j,50,50);
+                    airish.triangle(175+i,300+j,250+i,250+j,200+i,325+j);
+
+                    airish.ellipse(250+i,350+j,50,50);
+                    airish.triangle(225+i,350+j,250+i,250+j,275+i,350+j);
+
+                    airish.ellipse(325+i,325+j,50,50);
+                    airish.triangle(300+i,325+j,250+i,250+j,325+i,300+j);
+
+                    airish.fill(0);
+                    airish.ellipse(250+i,250+j,n,n);
+
+                }//end for
+
+            }//end for
+
+        }//end if
+
+        if(a>=2)
+        {
+            for (i=-125;i<1400;i+=250)
+            {
+                for (j=-125;j<700;j+=250)
+                {
+                    airish.fill (255);
+                    airish.ellipse (250+i,250+j,100,100); 
+                    airish.fill(200, 255, 255);
+
+                    airish.ellipse(175+i,175+j,50,50);
+                    airish.triangle(175+i,200+j,250+i,250+j,200+i,175+j);
+
+                    airish.ellipse(250+i,150+j,50,50);
+                    airish.triangle(225+i,150+j,250+i,250+j,275+i,150+j);
+
+                    airish.ellipse(325+i,175+j,50,50);
+                    airish.triangle(300+i,175+j,250+i,250+j,325+i,200+j);
+
+                    airish.ellipse(150+i,250+j,50,50);
+                    airish.triangle(150+i,225+j,250+1,250+j,150+1,275+j);
+
+                    airish.ellipse(350+i,250+j,50,50);
+                    airish.triangle(350+i,225+j,250+i,250+j,350+i,275+j);
+
+                    airish.ellipse(175+i,325+j,50,50);
+                    airish.triangle(175+i,300+j,250+i,250+j,200+i,325+j);
+
+                    airish.ellipse(250+i,350+j,50,50);
+                    airish.triangle(225+i,350+j,250+i,250+j,275+i,350+j);
+
+                    airish.ellipse(325+i,325+j,50,50);
+                    airish.triangle(300+i,325+j,250+i,250+j,325+i,300+j);
+
+                }//end for
+
+            }//end for
+
+        }//end if
 
 
-
-
-
-
-
-
-        //draw butterfly
-        airish.stroke (255) ;
-        airish.strokeWeight(2);
-    
-        airish.stroke (255) ;
-        airish.strokeWeight(3);
-        airish.fill(c, 255, 255);
-        airish.quad (airish.mouseX-60, airish.mouseY-70, airish.mouseX-10, airish.mouseY-50,airish.mouseX,airish.mouseY,airish.mouseX-40,airish.mouseY-10);
-        airish.quad (airish.mouseX+60, airish.mouseY-70, airish.mouseX+10, airish.mouseY-50, airish.mouseX, airish.mouseY, airish.mouseX+40, airish.mouseY-10);
-        airish.fill (c, 255, 255);
-        airish.quad (airish.mouseX-60, airish.mouseY+40, airish.mouseX-40, airish.mouseY, airish.mouseX, airish.mouseY, airish.mouseX-18, airish.mouseY+30);
-        airish.quad (airish.mouseX+60, airish.mouseY+40, airish.mouseX+40, airish.mouseY, airish.mouseX, airish.mouseY, airish.mouseX+10, airish.mouseY+30);
         
+
+       //draw butterfly
+       airish.stroke (255) ;
+       airish.strokeWeight(2);
+    
+       airish.stroke (255) ;
+       airish.strokeWeight(3);
+       airish.fill(c, 255, 255);
+       airish.quad (airish.mouseX-60, airish.mouseY-70, airish.mouseX-10, airish.mouseY-50,airish.mouseX,airish.mouseY,airish.mouseX-40,airish.mouseY-10);
+       airish.quad (airish.mouseX+60, airish.mouseY-70, airish.mouseX+10, airish.mouseY-50, airish.mouseX, airish.mouseY, airish.mouseX+40, airish.mouseY-10);
+       airish.fill (c, 255, 255);
+       airish.quad (airish.mouseX-60, airish.mouseY+40, airish.mouseX-40,airish.mouseY, airish.mouseX, airish.mouseY, airish.mouseX-18, airish.mouseY+30);
+       airish.quad (airish.mouseX+60, airish.mouseY+40, airish.mouseX+40,airish.mouseY, airish.mouseX, airish.mouseY, airish.mouseX+10,airish.mouseY+30);
         
     
-    }
 
+        
+        //moving the background 
+        n-=0.1f;
+        if(n==-0.5f)
+        {
+            n = 1;
+
+        }//end if
+        
+        //
+        a++;
+    }//end render()
     
+
 }//end main class
+    
