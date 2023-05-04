1e6dbcb1933f8764a5092c23015b08bcf96c0ee8
paris le <c20401536@mytudublin.ie>
2023-05-04 02:37:48 +0100

    finally fixed my code with newer sketch.. hopefully it be the last one

--- java/src/oopBaddies/paris.java
+++ java/src/oopBaddies/paris.java
@@ -2,150 +2,171 @@ package oopBaddies;
 
 import processing.core.PApplet;
 
-
 import ie.tudublin.Visual;
 
+public class paris extends Visual {
 
-public class paris extends Visual{
-    
+  Start paris;
 
-    Start paris;
+  public paris(Start paris) {
 
-    public paris(Start paris) {
-
-        this.paris = paris;
-    }
-    
-    
-    int cols, rows;
-    int scl = 30;
-    int w = 6000;
-    int h = 1900;
-
-    
-    float wave = 0;
-    
-    float[][] landscape;
-    
-
-    //Start of the draw render
-    void render() {
-  cols = w/scl;
-  rows = h/scl;
-  landscape = new float [cols][rows];
-
-  wave -= 0.1;
-  float yoff = wave;
-  for (int y = 0; y < rows; y++) {
-    float xoff = 0;
-    for (int x = 0; x< cols; x++) {
-      landscape[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-      xoff += 0.2;
-    }
-    yoff += 0.2;
-}
-
-paris.background(10, 20, 50);
-paris.stroke(255);
-paris.noFill();
-
-paris.fill(random(255), random(255), random(255),300);
-paris.noStroke();
-paris.ellipse(random(w), random(h), 20, 20);
-
-paris.pushMatrix();
-paris.translate(width/2, height/2+50);
-paris.rotateX(PI/3);
-paris.frameRate(1);
-
-// Colouring the landscape
-paris.noStroke();
-paris.fill(255, 0, 255, 100);
-
-paris.translate(-w/2, -h/2);
-for (int y = 0; y < rows-1; y++) {
-    paris.beginShape(PApplet.TRIANGLE_STRIP);
-    for (int x = 0; x< cols; x++) {
-      paris.vertex(x*scl, y*scl, landscape[x][y]);
-      paris.vertex(x*scl, (y+1)*scl, landscape[x][y+1]);
-    }
-    paris.endShape();
+    this.paris = paris;
   }
-  
-paris.popMatrix();
 
-// Draw grey circle in the center
+  // declare the variable
 
-// Draw orange sphere on the top
-paris.pushMatrix();
-paris.translate(width/2, height/2-400);
-paris.fill(255, 200, 0, 200);
-paris.noStroke();
-paris.sphere(140);
-paris.popMatrix();
+  int cols, rows;
+  int scl = 20;
+  int w = 6000;
+  int h = 1900;
 
-// the sun glistening
+  float wave = 0;
+  float[][] ocean;
 
-paris.pushMatrix();
-paris.translate(width/2, height/2-400);
-paris.rotateZ(PI/7);
-paris.fill(255, 200, 0, 200);
-paris.noStroke();
-paris.sphere(140);
+  // declare the plane variable
 
-paris.fill(255, 255, 255, 50);
-paris.rotateX(PI/2);
-paris.popMatrix();
+  int planeX = 0; 
+  int planeY = height / 5; 
 
-// draw hearts
+  // this is the start of the draw render
 
-paris.pushMatrix();
-paris.translate(width/2+950, height/2-300);
-paris.fill(random(255), random(255), random(255), 150);
-paris.noStroke();
-paris.beginShape();
-paris.vertex(0, -50, 0);
-paris.bezierVertex(0, -80, -30, -100, -60, -100);
-paris.bezierVertex(-90, -100, -90, -50, -90, -50);
-paris.bezierVertex(-90, -20, -70, 0, 0, 30);
-paris.bezierVertex(70, 0, 90, -20, 90, -50);
-paris.bezierVertex(90, -50, 90, -100, 60, -100);
-paris.bezierVertex(30, -100, 0, -80, 0, -50);
-paris.endShape(CLOSE);
-paris.popMatrix();
+  void render() {
 
-paris.pushMatrix();
-paris.translate(width/2-950, height/2-300);
-paris.fill(random(255), random(255), random(255), 150);
-paris.noStroke();
-paris.beginShape();
-paris.vertex(0, -50, 0);
-paris.bezierVertex(0, -80, -30, -100, -60, -100);
-paris.bezierVertex(-90, -100, -90, -50, -90, -50);
-paris.bezierVertex(-90, -20, -70, 0, 0, 30);
-paris.bezierVertex(70, 0, 90, -20, 90, -50);
-paris.bezierVertex(90, -50, 90, -100, 60, -100);
-paris.bezierVertex(30, -100, 0, -80, 0, -50);
-paris.endShape(CLOSE);
-paris.popMatrix();
+    // calulate average
 
-// testing
-
-paris.pushMatrix();
-paris.translate(+700, +110);
-paris.fill(random(255), random(255), random(255));
-paris.noStroke();
-paris.beginShape();
-paris.vertex(0, -50, 0);
-paris.bezierVertex(0, -80, -30, -100, -60, -100);
-paris.bezierVertex(-90, -100, -90, -50, -90, -50);
-paris.bezierVertex(-90, -20, -70, 0, 0, 30);
-paris.bezierVertex(70, 0, 90, -20, 90, -50);
-paris.bezierVertex(90, -50, 90, -100, 60, -100);
-paris.bezierVertex(30, -100, 0, -80, 0, -50);
-paris.endShape(CLOSE);
-paris.popMatrix();
-
+    float avg = 0;
+    for (int i = 0; i < ab.size(); i++) {
+      avg += abs(ab.get(i));
     }
+    avg = avg / ab.size();
+    float smoothedavg = 0;
+    smoothedavg = lerp(smoothedavg, avg, 0.1f);
+
+    // variable for the ocean wave
+
+    cols = w / scl;
+    rows = h / scl;
+    ocean = new float[cols][rows];
+
+    // wave movement setting
+
+    wave -= 0.04;
+    float yoff = wave;
+    for (int y = 0; y < rows; y++) {
+      float xoff = 0;
+      for (int x = 0; x < cols; x++) {
+        ocean[x][y] = map(noise(xoff, yoff), 0, 1, -300, 20);
+        xoff += 0.06;
+      }
+      yoff += 0.06;
+    }
+
+    // colouring of the background
+
+    paris.background(10, 20, 50);
+    paris.stroke(255);
+    paris.noFill();
+
+    // this is to add the rainbow pixel effect
+
+    paris.fill(random(255), random(255), random(255), 300);
+    paris.noStroke();
+    paris.rect(random(w), random(h), 25, 25);
+
+    // adding shading to the ocean wave
+
+    paris.pushMatrix();
+    paris.translate(width / 2, height / 2 + 2);
+    paris.rotateX(PI / 3);
+
+    // colouring of the blue ocean
+
+    paris.noStroke();
+    paris.fill(0, 191, 255, 100);
+    paris.frameRate(15);
+
+    // wave movement setting
+
+    paris.translate(-w / 2, +300);
+    for (int y = 0; y < rows - 1; y++) {
+      paris.beginShape(PApplet.TRIANGLE_STRIP);
+      for (int x = 0; x < cols; x++) {
+        paris.vertex(x * scl, y * scl, ocean[x][y]);
+        paris.vertex(x * scl, (y + 1) * scl, ocean[x][y + 1]);
+      }
+      paris.endShape();
+    }
+
+    paris.popMatrix();
+
+    // drawing of the moon
+
+    paris.pushMatrix();
+    paris.translate(+1000, +130);
+    paris.fill(250, 250, 200, 200);
+    paris.noStroke();
+    paris.ellipse(width - 100, height - 25, 400 + (smoothedavg * 600), 400 + (smoothedavg * 600));
+    paris.popMatrix();
+
+    // drawing of the left rainbow heart
+
+    paris.pushMatrix();
+    paris.translate(+500, +160);
+    paris.fill(random(255), random(255), random(255));
+    paris.noStroke();
+    paris.beginShape();
+    paris.vertex(0, -50, 0);
+    paris.bezierVertex(0, -80, -30, -100, -60, -100);
+    paris.bezierVertex(-90, -100, -90, -50, -90, -50);
+    paris.bezierVertex(-90, -20, -70, 0, 0, 30);
+    paris.bezierVertex(70, 0, 90, -20, 90, -50);
+    paris.bezierVertex(90, -50, 90, -100, 60, -100);
+    paris.bezierVertex(30, -100, 0, -80, 0, -50);
+    paris.endShape(CLOSE);
+    paris.popMatrix();
+
+    // drawing of the right rainbow heart
+
+    paris.pushMatrix();
+    paris.translate(1500, +160);
+    paris.fill(random(255), random(255), random(255));
+    paris.noStroke();
+    paris.beginShape();
+    paris.vertex(0, -50, 0);
+    paris.bezierVertex(0, -80, -30, -100, -60, -100);
+    paris.bezierVertex(-90, -100, -90, -50, -90, -50);
+    paris.bezierVertex(-90, -20, -70, 0, 0, 30);
+    paris.bezierVertex(70, 0, 90, -20, 90, -50);
+    paris.bezierVertex(90, -50, 90, -100, 60, -100);
+    paris.bezierVertex(30, -100, 0, -80, 0, -50);
+    paris.endShape(CLOSE);
+    paris.popMatrix();
+
+    // drawing plane that fly across the screen
+
+    paris.pushMatrix();
+    paris.translate(0, 160);
+    paris.fill(200);
+    paris.stroke(64, 64, 64, 128);
+    paris.strokeWeight(2);
+    paris.beginShape();
+    paris.vertex(planeX, planeY);
+    paris.vertex(planeX + 40, planeY - 20);
+    paris.vertex(planeX + 120, planeY);
+    paris.vertex(planeX + 40, planeY + 20);
+    paris.endShape(CLOSE);
+    paris.fill(128, 192, 255, 128);
+    paris.stroke(64, 64, 64, 128);
+    paris.ellipse(planeX + 50, planeY - 5, 55, 25);
+    paris.popMatrix();
+
+    // update plane's position
     
+    planeX += 3;
+    if (planeX > width + 1800) { // reset plane's position when it goes off screen
+      planeX = -1000;
+
     }
+
+  }
+}
